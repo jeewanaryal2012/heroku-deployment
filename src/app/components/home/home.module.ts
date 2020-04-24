1ae3143e0efddd040cc4882a3bfb4974c7aefe65
jeewanaryal <jeewanaryal@gmail.com>
2020-04-23 20:16:36 -0500

    u

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -4,10 +4,14 @@ import { UniversalModule } from '../../common/universal/universal.module';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { CreateAdComponent } from '../../authorized/create-ad/create-ad.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    CreateAdComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule,
