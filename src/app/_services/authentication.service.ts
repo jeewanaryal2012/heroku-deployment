59822f3ae702535345cbc785f44f69eb31a8e134
jeewanaryal <jeewanaryal@gmail.com>
2020-04-26 18:59:20 -0500

    jwt auth

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -41,7 +41,7 @@ export class AuthenticationService {
 
   logout() {
     // remove user from local storage to log user out
-    localStorage.removeItem('currentUser');
+    localStorage.removeItem('jwt');
     this.currentUserSubject.next(null);
   }
 }
--- src/app/components/home/menu/menu.component.ts
+++ src/app/components/home/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from '../../../_services/authentication.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router, private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
   }
 
+  logout(e) {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 }
